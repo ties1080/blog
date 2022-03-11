99ba54b87ff9b757d7d34f852e841dcd47f3a6d1
Tiecoura SAMAKE <ties1080@gmail.com>
2022-03-11 15:28:02 +0000

    comments service init

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { bodyParser } = require('body-parser');
-const { randomBytes } =require('crypto');
+const { randomBytes } = require('crypto');
 
+const bodyParser = require('body-parser');
 
-const  app = express();
+const app = express();
 app.use(bodyParser.json());
 
 
