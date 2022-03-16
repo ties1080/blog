6b8e425481d5f446f92302c8f1c5eeb2bafa3709
Tiecoura SAMAKE <ties1080@gmail.com>
2022-03-16 18:01:17 +0000

    with event-bus

--- comments/index.js
+++ comments/index.js
@@ -2,22 +2,42 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 
 const commentsPostId = {}
 
-app.get('/posts/:id/comments', (req, res)=>{
-
+app.get('/posts/:id/comments', (req, res) => {
+    res.status(201).send(commentsPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req,res)=>{
+app.post('/posts/:id/comments', async (req,res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body;
 
+    const comments = commentsPostId[req.params.id] || []
+
+    comments.push({id: commentId, content});
+
+    commentsPostId[req.params.id] = comments;
+
+    await axios.post('https://localhost:4005/events', {
+        type:'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId:req.params.id,
+        }
+    });
+
+    res.status(201).send(comments);
 })
 
 
-app.listen(4001, ()=>{
+app.listen(4001, () => {
     console.log('Comment service listening on 4001');
 })
